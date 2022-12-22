3e0385c7bb5d80a4b3cc6e209d78a7aed64fbf99
test <test@test.com>
2022-12-22 18:44:37 +0900

    Spring MyBatis 연동 퀴즈

---
+++ src/main/java/com/quiz/lesson02/Lesson02Quiz01RestController.java
@@ -0,0 +1,10 @@
+package com.quiz.lesson02;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Lesson02Quiz01RestController {
+
+	// http://localhost/lesson02/quiz01
+	@RequestMapping("")
+}
